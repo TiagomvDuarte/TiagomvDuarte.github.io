c3c0227f35f2652e8220f721becb9f1bc823b5dc
Tiago Duarte <tmv.duarte@campus.fct.unl.pt>
2024-07-05 08:43:54 +0100

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,9 @@
+function showSideBar() {
+  const sideBar = document.querySelector('.sideBar')
+  sideBar.style.display = 'flex'
+}
+
+function hideSideBar() {
+  const sideBar = document.querySelector('.sideBar')
+  sideBar.style.display = 'none'
+}
